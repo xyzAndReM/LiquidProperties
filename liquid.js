0649baf48899ed985856757d37a4fce82b8efab8
Andre <a.marcello92@gmail.com>
2018-06-18 00:27:37 -0300

    Odeio minha vida

--- liquid.js
+++ liquid.js
@@ -41,42 +41,160 @@ var dyethil_ether = {
          }
       }
 }
+var hexene = {
+   "name":"Hexene",
+   "properties":
+      {
+         "viscosity":{
+            "A":"-3.71089",
+            "B":"596.135",
+            "C":"-43.7713",
+            "Tmin":"158",
+            "Tmax":"373"
+         },
+         "density":{
+            "A":"10.0696",
+            "B":"0.101281",
+            "C":"1995.46",
+            "D":"1.1334",
+            "Tmin":"146",
+            "Tmax":"325"
+         }
+      }
+}
+var diol = {
+   "name":"Ethane diol",
+   "properties":
+      {
+         "viscosity":{
+            "A":"-3.44553",
+            "B":"946.08",
+            "C":"-147.014",
+            "Tmin":"253",
+            "Tmax":"353"
+         },
+         "density":{
+            "A":"40.4538",
+            "B":"0.177155",
+            "C":"753.782",
+            "D":"0.179703",
+            "Tmin":"250",
+            "Tmax":"473"
+         }
+      }
+}
+var propano_diol = {
+   "name":"Propane diol",
+   "properties":
+      {
+         "viscosity":{
+            "A":"-2.43342",
+            "B":"625.296",
+            "C":"-197.07",
+            "Tmin":"288",
+            "Tmax":"323"
+         },
+         "density":{
+            "A":"71.7956",
+            "B":"0.240024",
+            "C":"851.803",
+            "D":"0.32772",
+            "Tmin":"243",
+            "Tmax":"423"
+         }
+      }
+}
 
-var liquids = {'Ethanol':ethanol,'Dyethil Ether':dyethil_ether};
+var liquids = {'Ethanol':ethanol,'Dyethil Ether':dyethil_ether,'Hexene':hexene,'Ethane Diol':diol,'Propane Diol':propano_diol};
 
 
 function  Viscosity(liquid,temperature){
         var A = parseFloat(liquid.properties.viscosity.A);
         var B = parseFloat(liquid.properties.viscosity.B);
         var C = parseFloat(liquid.properties.viscosity.C);
-        var Tmin = parseInt(liquid.properties.viscosity.Tmin);
-        var Tmax = parseInt(liquid.properties.viscosity.Tmax);
         var T = temperature;
-        if( (T >= Tmin) && (T <= Tmax) ){
-            return "$ \\text{Viscosidade Dinâmica } = " + Math.exp(A + B/(C+T)).toFixed(6) +"\\;\\;mP_a\\,s$";
-        }
-        else return "Temperatura fora do limite (" + Tmin + ',' + Tmax + ')';
+        return Math.exp(A + B/(C+T)).toFixed(6);
+}
+function Viscosity_Text(liquid,temperature){
+    var Tmin = parseInt(liquid.properties.density.Tmin);
+    var Tmax = parseInt(liquid.properties.density.Tmax);
+    var T = temperature;
+    if( (T >= Tmin) && (T <= Tmax) ){
+        return "$ \\text{Viscosidade Dinâmica } = " + Viscosity(liquid,temperature) +"\\;\\;mP_a\\,s$";
     }
+    else return "Temperatura fora do limite (" + Tmin + ',' + Tmax + ')';
+}
+function Kinetic_Viscosity(liquid,temperature){
+    var V = parseFloat(Viscosity(liquid,temperature));
+    var D = parseFloat(Density(liquid,temperature));
+    return 1000*V/D;
+}
+function Kinetic_Viscosity_Text(liquid,temperature){
+    var Tmin = parseInt(liquid.properties.density.Tmin);
+    var Tmax = parseInt(liquid.properties.density.Tmax);
+    var T = temperature;
+    return "$ \\text{Viscosidade cinética } = " + Kinetic_Viscosity(liquid,temperature) +"\\;\\;cS$";
+}
 function Density(liquid,temperature){
         var A = parseFloat(liquid.properties.density.A);
         var B = parseFloat(liquid.properties.density.B);
         var C = parseFloat(liquid.properties.density.C);
         var D = parseFloat(liquid.properties.density.D);
-        var Tmin = parseInt(liquid.properties.density.Tmin);
-        var Tmax = parseInt(liquid.properties.density.Tmax);
         var T = temperature;
-        if( (T >= Tmin) && (T <= Tmax) ){
-            return "$ \\text{Densidade } = " + (A/Math.pow(B,(1 + Math.pow((1 - T/C),D) ))).toFixed(6) +"\\;\\;kg\\,m^{-3}$";
-        }
-        else return "Temperatura fora do limite (" + Tmin + ',' + Tmax + ')';
-
+        return  (A/Math.pow(B,(1 + Math.pow((1 - T/C),D) ))).toFixed(6);
 }
+function Density_Text(liquid,temperature){
+    var Tmin = parseInt(liquid.properties.density.Tmin);
+    var Tmax = parseInt(liquid.properties.density.Tmax);
+    var T = temperature;
+    if( (T >= Tmin) && (T <= Tmax) ){
+        return "$ \\text{Densidade } = " + Density(liquid,temperature) +"\\;\\;kg\\,m^{-3}$";
+    }
+    else return "Temperatura fora do limite (" + Tmin + ',' + Tmax + ')';
+}
+function VBN(lA,lB,xA,xB,T){
+   var vA = Kinetic_Viscosity(lA,T);
+   var vB = Kinetic_Viscosity(lB,T);
+   //console.log(vA+0.8);
+   //console.log(Math.log(vA+0.8));
+   var vbnA = 14.534*Math.log(Math.log(vA+0.8))+10.975;
+   console.log(vbnA);
+   var vbnB = 14.534*Math.log(Math.log(vB+0.8))+10.975;
+   console.log(vbnB);
+   console.log(xA);
+   console.log(xB);
+   var vbnM = xA*vbnA + xB*vbnB;
+   console.log(vbnM);
+   console.log(Math.exp((vbnM - 10.975)/14.534));
+   var vM = Math.exp(Math.exp((vbnM - 10.975)/14.534)) - 0.8;
+   console.log(vM)
+   return vM;
+}
+
+function VBN_Text(lA,lB,xA,xB,T){
+    return "$ \\text{Viscosidade Cinética } = " + VBN(lA,lB,xA,xB,T) +"\\;\\;mP_a\\,s$";
+}
+
+//function Mixture_Viscosity()
+
 function Output(){
-    var liquid = liquids[(document.getElementsByName("liquido")[0].value)];
+    var liquid1 = liquids[(document.getElementsByName("liquido1")[0].value)];
+    var liquid2 = liquids[(document.getElementsByName("liquido2")[0].value)];
+    var x1 = parseFloat((document.getElementsByName("xA")[0].value));
+    var x2 = parseFloat(1 - x1);
     var temperature = Number(document.getElementsByName("temperature")[0].value);
     var texto = '';
-    texto += Viscosity(liquid,temperature) + "<br>";
-    texto += Density(liquid,temperature) + "<br>";
+    texto += "Líquido 1 <br>";
+    texto += Viscosity_Text(liquid1,temperature) + " <br>";
+    texto += Density_Text(liquid1,temperature) + " <br>";
+    texto += Kinetic_Viscosity_Text(liquid1,temperature) + " <br>"
+    texto += "Líquido 2 <br>"
+    texto += Viscosity_Text(liquid2,temperature) + " <br>";
+    texto += Density_Text(liquid2,temperature) + " <br>";
+    texto += Kinetic_Viscosity_Text(liquid2,temperature) + " <br>"
+    texto += "Mistura <br>";
+    texto += VBN_Text(liquid1,liquid2,x1,x2,temperature) + " <br>";
+    //texto += Mixture_Viscosity(liquid1,liquid2);
     document.getElementById("respostas").innerHTML = texto;
     MathJax.Hub.Queue(["Typeset",MathJax.Hub,"respostas"]);
 
